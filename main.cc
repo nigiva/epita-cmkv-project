35b6a448896b9f915a42b4f0254286bc05790a53
Corentin Duchêne <corentin.duchene@epita.fr>
2023-01-20 22:17:42 +0100

    feat: multi-thread with future

--- main.cc
+++ main.cc
@@ -1,9 +1,15 @@
+#include <atomic>
 #include <fstream>
+#include <future>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <vector>
 
 #include "board.hh"
 #include "piece.hh"
+
+const int numWorkers = 2;
 
 std::string readFile(std::string filepath)
 {
@@ -23,8 +29,62 @@ std::string readFile(std::string filepath)
     return content;
 }
 
+std::optional<std::string> worker(std::string inputBoardContent)
+{
+    Board board = Board::fromString(inputBoardContent);
+    auto success = board.solve(1'000'000, // epochMax
+                               1, // initialTemperature
+                               0.9999, // decayRate
+                               false // verbose
+    );
+    if (success)
+    {
+        return "success";
+    }
+    else
+    {
+        //
+        std::cerr << "Worker finish but not success (loss=" << board.loss()
+                  << ")" << std::endl;
+        return std::nullopt;
+    }
+}
+
+std::string runWorkers(std::string inputBoardContent)
+{
+    std::vector<std::future<std::optional<std::string>>> futureList;
+    std::string result;
+    for (int i = 0; i < numWorkers; i++)
+    {
+        futureList.push_back(
+            std::async(std::launch::async, worker, inputBoardContent));
+    }
+
+    int numberWorkersFinished = 0;
+    while (numberWorkersFinished < numWorkers)
+    {
+        numberWorkersFinished = 0;
+        for (auto &future : futureList)
+        {
+            if (future.wait_for(std::chrono::seconds(0))
+                == std::future_status::ready)
+            {
+                numberWorkersFinished++;
+                auto result = future.get();
+                if (result.has_value())
+                {
+                    return result.value();
+                }
+            }
+        }
+    }
+    std::cerr << "Unable to solve the board" << std::endl;
+    exit(1);
+}
+
 int main(int argc, char **argv)
 {
+    // Check arguments and parse them
     bool defaultArgsCondition = (argc == 3);
     bool debugArgsCondition = (argc == 4 && std::string(argv[3]) == "--debug");
     bool debug = debugArgsCondition;
@@ -40,29 +100,18 @@ int main(int argc, char **argv)
     std::string inputPath = argv[1];
     std::string outputPath = argv[2];
 
+    // Read the input board
     std::string inputBoardContent = readFile(inputPath);
-    Board board = Board::fromString(inputBoardContent);
-    if (debug)
-    {
-        std::cout << board << std::endl;
-    }
 
-    auto success = board.solve(1'000'000, // epochMax
-                               1, // initialTemperature
-                               0.9999, // decayRate
-                               debug // verbose
-    );
+    // Run all threads to solve the board
+    std::string result = runWorkers(inputBoardContent);
+
+    // Write the solution to the output file
     if (debug)
     {
         std::cout << "\n---  RESULT  ---" << std::endl;
-        std::cout << board << std::endl;
+        std::cout << result << std::endl;
     }
 
-    if (!success)
-    {
-        std::cerr << "Unable to solve the board (loss=" << board.loss() << ")"
-                  << std::endl;
-        return 1;
-    }
     return 0;
 }
